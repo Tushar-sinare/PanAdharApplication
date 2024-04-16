5c66ce45a84d0d9dc07d59120cca97adf4fd42e4
Tushar-sinare <tusharsinare1995@gmail.com>
2024-04-16 11:25:15 +0530

    add

--- src/main/java/com/netwin/config/ConnectionFactory.java
+++ src/main/java/com/netwin/config/ConnectionFactory.java
@@ -13,29 +13,28 @@ import org.apache.commons.dbcp.PoolingDataSource;
 import org.apache.commons.pool.impl.GenericObjectPool;
 public class ConnectionFactory
 {
-	private static interface Singleton {
-        final ConnectionFactory INSTANCE = new ConnectionFactory();
-    }
+	/*
+	 * private static interface Singleton { final ConnectionFactory INSTANCE = new
+	 * ConnectionFactory(); }
+	 */
  
-    private final DataSource dataSource;
- 
-    private ConnectionFactory() {
-        Properties properties = new Properties();
-        properties.setProperty("user", "root");
-        properties.setProperty("password", "Netwin@123"); // or get properties from some configuration file
- 
-        GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>();
-        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
-                "jdbc:mysql://localhost:3307/pnadha", properties
-        );
-        new PoolableConnectionFactory(
-                connectionFactory, pool, null, "SELECT 1", 3, false, false, Connection.TRANSACTION_READ_COMMITTED
-        );
- 
-        this.dataSource = new PoolingDataSource(pool);
-    }
- 
-    public static Connection getDatabaseConnection() throws SQLException {
-        return Singleton.INSTANCE.dataSource.getConnection();
-    }
+	/*
+	 * private final DataSource dataSource;
+	 * 
+	 * private ConnectionFactory() { Properties properties = new Properties();
+	 * properties.setProperty("user", "root"); properties.setProperty("password",
+	 * "Netwin@123"); // or get properties from some configuration file
+	 * 
+	 * GenericObjectPool<PoolableConnection> pool = new
+	 * GenericObjectPool<PoolableConnection>(); DriverManagerConnectionFactory
+	 * connectionFactory = new DriverManagerConnectionFactory(
+	 * "jdbc:mysql://localhost:3307/pnadha", properties ); new
+	 * PoolableConnectionFactory( connectionFactory, pool, null, "SELECT 1", 3,
+	 * false, false, Connection.TRANSACTION_READ_COMMITTED );
+	 * 
+	 * this.dataSource = new PoolingDataSource(pool); }
+	 * 
+	 * public static Connection getDatabaseConnection() throws SQLException { return
+	 * Singleton.INSTANCE.dataSource.getConnection(); }
+	 */
 }
