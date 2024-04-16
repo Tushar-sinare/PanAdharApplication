490c1f5cce593d25b6d2d0cfd2e5ca50609a1ac7
Tushar-sinare <tusharsinare1995@gmail.com>
2024-04-16 13:14:38 +0530

    add new changes

--- src/main/java/com/netwin/controller/PnNetwinRequestController.java
+++ src/main/java/com/netwin/controller/PnNetwinRequestController.java
@@ -9,30 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.netwin.service.PnNetwinRequestService;
 
-import com.netwin.util.PnNetwinDecrypt;
+
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class PnNetwinRequestController {
-	
-	private PnNetwinDecrypt pnNetwinDecrypt;
+
 
 	private PnNetwinRequestService pnNetwinRequestService;
 
 	@PostMapping("/pnrequest")
-	public ResponseEntity<String> callPanRequest(@RequestBody String panRequestJson, HttpServletRequest request)
-			throws Exception {
-		long startTime = System.currentTimeMillis();
+	public ResponseEntity<String> callPanRequest(@RequestBody String panRequestJson, HttpServletRequest request){
+		
 		String clientIp = request.getRemoteAddr();
 
 		try {
 			if (panRequestJson != null && !panRequestJson.isEmpty()) {
 				String pnRequestStr = pnNetwinRequestService.callPanRequest(panRequestJson, clientIp);
-				long endTime = System.currentTimeMillis();
-
-				long responseTime = endTime - startTime;
-				System.err.println(responseTime);
+								
 				return new ResponseEntity<String>(pnRequestStr, HttpStatus.ACCEPTED);
 			}
 		} catch (Exception ex) {
