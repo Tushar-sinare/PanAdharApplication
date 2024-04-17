8a39fd1475d9a850fbb2fc48896d83c6c0e5edbe
Tushar-sinare <tusharsinare1995@gmail.com>
2024-04-17 11:01:50 +0530

    add new changes

--- src/main/java/com/netwin/service/impl/PnVndrRequestServiceImpl.java
+++ src/main/java/com/netwin/service/impl/PnVndrRequestServiceImpl.java
@@ -23,62 +23,65 @@ import com.netwin.validation.PnVndrValidation;
 @Service
 public class PnVndrRequestServiceImpl implements PnVndrRequestService {
 
-private final PnVndrValidation pnVndrValidation;
+	private final PnVndrValidation pnVndrValidation;
 
-private final PnVndrResponseService pnVndrResponseService;
+	private final PnVndrResponseService pnVndrResponseService;
 
-private final PnNetwinDecrypt pnNetwinDecrypt;
+	private final PnNetwinDecrypt pnNetwinDecrypt;
 
-private final PnVndrRequestRepo pnVndrRequestRepo;
+	private final PnVndrRequestRepo pnVndrRequestRepo;
 
-private final ErrorApplicationService errorApplicationService;
-@Autowired
-public PnVndrRequestServiceImpl(PnVndrValidation pnVndrValidation, PnVndrResponseService pnVndrResponseService,PnNetwinDecrypt pnNetwinDecrypt,PnVndrRequestRepo pnVndrRequestRepo,ErrorApplicationService errorApplicationService) {
-	this.pnVndrValidation=pnVndrValidation;
-	this.pnVndrResponseService=pnVndrResponseService;
-	this.pnNetwinDecrypt =pnNetwinDecrypt;
-	this.pnVndrRequestRepo= pnVndrRequestRepo;
-	this.errorApplicationService = errorApplicationService;
-}
-private static final Logger logger = LoggerFactory.getLogger(PnVndrRequestServiceImpl.class);
-	@Override
-	public Result<String> fetchPnVndrRequest(PnRequest pnReqData,Map<String, String> pnRequestDecrypt) {
-	
-		
-		
-		 Map<String,String> vendorValue=null;
-		 //Check Vendor Mapping
-		Result<PnRequest> result = pnVndrValidation.checkMappingVendor(pnReqData,pnRequestDecrypt);
-		if (result.isValid()) {
-			vendorValue =result.getMap();
-			//call Vendor Request Store method
-			
-			String pnVndrResponse = callVendorMethod(vendorValue,pnReqData);
-			return new Result<>(pnVndrResponse);
-			 } else {
-			    String errorMessage = result.getErrorMessage();
-			    // Handle the error message
-			    logger.error(errorMessage);
-			    errorApplicationService.storeError(109, errorMessage);
-			  return new Result<>(errorMessage);
-			}
-		
+	private final ErrorApplicationService errorApplicationService;
+
+	@Autowired
+	public PnVndrRequestServiceImpl(PnVndrValidation pnVndrValidation, PnVndrResponseService pnVndrResponseService,
+			PnNetwinDecrypt pnNetwinDecrypt, PnVndrRequestRepo pnVndrRequestRepo,
+			ErrorApplicationService errorApplicationService) {
+		this.pnVndrValidation = pnVndrValidation;
+		this.pnVndrResponseService = pnVndrResponseService;
+		this.pnNetwinDecrypt = pnNetwinDecrypt;
+		this.pnVndrRequestRepo = pnVndrRequestRepo;
+		this.errorApplicationService = errorApplicationService;
 	}
+
+	private static final Logger logger = LoggerFactory.getLogger(PnVndrRequestServiceImpl.class);
+
+	@Override
+	public Result<String> fetchPnVndrRequest(PnRequest pnReqData, Map<String, String> pnRequestDecrypt) {
+
+		Map<String, String> vendorValue = null;
+		// Check Vendor Mapping
+		Result<PnRequest> result = pnVndrValidation.checkMappingVendor(pnReqData, pnRequestDecrypt);
+		if (result.isValid()) {
+			vendorValue = result.getMap();
+			// call Vendor Request Store method
+
+			String pnVndrResponse = callVendorMethod(vendorValue, pnReqData);
+			return new Result<>(pnVndrResponse);
+		} else {
+			String errorMessage = result.getErrorMessage();
+			// Handle the error message
+			logger.error(errorMessage);
+			errorApplicationService.storeError(109, errorMessage);
+			return new Result<>(errorMessage);
+		}
+
+	}
+
 	private String callVendorMethod(Map<String, String> vendorValue, PnRequest pnRequest2) {
-	
-		PnVndrRequest pnVndrRequest=new PnVndrRequest();
+
+		PnVndrRequest pnVndrRequest = new PnVndrRequest();
 		PnNetwinRequest pnNetwinRequest = pnRequest2.getPnNetwinRequest();
 		pnVndrRequest.setCallingIpAdr(pnNetwinRequest.getCallingIpAdr());
 		pnVndrRequest.setEntryDate(pnRequest2.getAppDate());
 		pnVndrRequest.setReqDecrypt(vendorValue.toString());
 		pnVndrRequest.setReqEncrypt(pnNetwinDecrypt.getPnRequestEncryptData(vendorValue).toString());
 		PnVndrRequest pnVndrRequest2 = pnVndrRequestRepo.save(pnVndrRequest);
-		//Call Vendor Api to fetch Response
-		
-		Result1<PnResponse> pnVndrApiResponse = pnVndrResponseService.fetchPanApiResponse(pnVndrRequest2,pnRequest2);
-								
-				return  pnVndrApiResponse.getResMap().toString();
-		
+		// Call Vendor Api to fetch Response
+
+		Result1<PnResponse> pnVndrApiResponse = pnVndrResponseService.fetchPanApiResponse(pnVndrRequest2, pnRequest2);
+
+		return pnVndrApiResponse.getResMap().toString();
 
 	}
 
