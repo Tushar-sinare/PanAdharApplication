10c1dbe2245e262f4a9cbb4cfde7b328ecb7d964
Tushar-sinare <tusharsinare1995@gmail.com>
2024-04-17 11:05:09 +0530

    add new changes

--- src/main/java/com/netwin/util/AESExample.java
+++ src/main/java/com/netwin/util/AESExample.java
@@ -1,34 +1,31 @@
 package com.netwin.util;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class AESExample {
 
+    private AESExample() {
+        throw new IllegalStateException("Utility class");
+    }
+
     // Encrypt function
+    public static String encrypt(String plainText, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
 
-	  private AESExample() {
-		    throw new IllegalStateException("Utility class");
-		  }
-
-	  public static String encrypt(String plainText, String key) throws Exception{
-	  
-	  SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES"); 
-	  Cipher cipher = Cipher.getInstance("AES");
-	  cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-	  byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-	  return Base64.getEncoder().encodeToString(encryptedBytes); }
-	  
-	  // Decrypt function
-	  
-	  public static String decrypt(String encryptedText, String key)throws Exception{
-		  SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES"); 
-		  Cipher cipher = Cipher.getInstance("AES");
-	  cipher.init(Cipher.DECRYPT_MODE, secretKey); 
-	  byte[] cipherBytes = Base64.getDecoder().decode(encryptedText);
-	  byte[] decryptedBytes =  cipher.doFinal(cipherBytes);
-	  return new String(decryptedBytes);
-	  }
-	  
-		
+    // Decrypt function
+    public static String decrypt(String encryptedText, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] cipherBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(cipherBytes);
+        return new String(decryptedBytes);
+    }
 }
