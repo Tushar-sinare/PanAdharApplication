dc164f005aaa94b8ef9d90b3d0e07a03f1933876
Tushar-sinare <tusharsinare1995@gmail.com>
2024-04-17 12:05:34 +0530

    add new changes

--- src/main/java/com/netwin/controller/PnNetwinRequestController.java
+++ src/main/java/com/netwin/controller/PnNetwinRequestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netwin.service.ErrorApplicationService;
 import com.netwin.service.PnNetwinRequestService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,10 +17,11 @@ public class PnNetwinRequestController {
 
 	// Add this annotation to inject the service
 	private final PnNetwinRequestService pnNetwinRequestService;
-
+private final ErrorApplicationService errorApplicationService;
 	@Autowired
-	public PnNetwinRequestController(PnNetwinRequestService pnNetwinRequestService) {
+	public PnNetwinRequestController(PnNetwinRequestService pnNetwinRequestService,ErrorApplicationService errorApplicationService) {
 		this.pnNetwinRequestService = pnNetwinRequestService;
+		this.errorApplicationService = errorApplicationService;
 	}
 
 	@PostMapping("/pnrequest")
@@ -32,9 +34,11 @@ public class PnNetwinRequestController {
 				return new ResponseEntity<String>(pnRequestStr, HttpStatus.ACCEPTED);
 			}
 		} catch (Exception ex) {
-			ex.printStackTrace();
+			 errorApplicationService.storeError(401,ex.getMessage());
+			 return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_GATEWAY);
+			 
 		}
 
-		return new ResponseEntity<String>("Json is Empty", HttpStatus.BAD_GATEWAY);
+		return new ResponseEntity<String>("Json is Empty", HttpStatus.BAD_REQUEST);
 	}
 }
