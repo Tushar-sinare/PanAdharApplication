b1ff9f9e5baac70ff34b455c09354371658ff105
Tushar-sinare <tusharsinare0202@gmail.com>
2024-04-29 18:45:32 +0530

    Add all my files

--- src/main/java/com/netwin/service/PnResponseService.java
+++ src/main/java/com/netwin/service/PnResponseService.java
@@ -2,13 +2,11 @@ package com.netwin.service;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.netwin.dto.CustomerVendorDetailsDto;
 
 public interface PnResponseService {
 
-	//Map<String, Object> fetchNetwinResponse(PnVndrResponse pnResponse, PnRequest pnRequest2) throws  JsonProcessingException;
-
-	String customerResponseMapping(String vndrResponseStr, CustomerVendorDetailsDto customerVendorDetailsDto) throws JsonMappingException, JsonProcessingException;
+	
+	String customerResponseMapping(String vndrResponseStr, CustomerVendorDetailsDto customerVendorDetailsDto) throws JsonProcessingException;
 
 }
--- src/main/java/com/netwin/service/PnVndrRequestService.java
+++ src/main/java/com/netwin/service/PnVndrRequestService.java
@@ -4,14 +4,13 @@ package com.netwin.service;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.netwin.dto.CustomerVendorDetailsDto;
 
 public interface PnVndrRequestService {
 
-	//Result fetchPnVndrRequest(PnRequest pnRequest2,Map<String, String> pnRequestDecrypt);
 
-	String callVenderRequest(String vendorRequestJson, CustomerVendorDetailsDto customerVendorDetailsDto) throws JsonMappingException, JsonProcessingException;
+
+	String callVenderRequest(String vendorRequestJson, CustomerVendorDetailsDto customerVendorDetailsDto) throws JsonProcessingException;
 
 
 }
